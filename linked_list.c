ff83bf4cbce9b1bf4470753a8ef09f82c8f96a90
Camilla <ccheng6@stuy.edu>
2018-10-15 15:20:26 -0400

    edited indents cause it bothered me

--- linked_list.c
+++ linked_list.c
@@ -5,73 +5,73 @@
 
 // prints the linked list of the specified node element
 void print_list(struct song_node * node) {
-    struct song_node * temp = node;
-    if(! temp) {
-        printf("[]\n");
+  struct song_node * temp = node;
+  if(! temp) {
+    printf("[]\n");
+  } else {
+    if(temp -> next == NULL) {
+      printf("[%s, %s] -> [NULL]\n", temp -> artist, temp -> name);
+      return;
     } else {
-        if(temp -> next == NULL) {
-            printf("[%s, %s] -> [NULL]\n", temp -> artist, temp -> name);
-            return;
-        } else {
-            printf("[%s, %s] -> ", temp -> artist, temp -> name);
-            return(print_list(temp -> next));
-        }
+      printf("[%s, %s] -> ", temp -> artist, temp -> name);
+      return(print_list(temp -> next));
     }
+  }
 }
 
 // creates and returns a pointer with a value of the input
 struct song_node * create(char * a, char * n) {
-    struct song_node * temp;
-    temp = malloc(sizeof(struct song_node));
-    strcpy(temp -> artist, a);
-    strcpy(temp -> name, n);
-    temp -> next = NULL;
-    return temp;
+  struct song_node * temp;
+  temp = malloc(sizeof(struct song_node));
+  strcpy(temp -> artist, a);
+  strcpy(temp -> name, n);
+  temp -> next = NULL;
+  return temp;
 }
 
 // insert to the front of input node a new node with value x.
 // return the new linked list
 struct song_node * insert_front(struct song_node * node, char * a, char * n) {
-    struct song_node * temp = node;
-    if(temp -> next == NULL) {
-        temp -> next = create(a, n);
-        return temp;
-    } else {
-        return(insert_front(temp -> next, a, n));
-    }
+  struct song_node * temp = node;
+  if(temp -> next == NULL) {
+    temp -> next = create(a, n);
+    return temp;
+  } else {
+    return(insert_front(temp -> next, a, n));
+  }
 }
 
 struct song_node * insert_order(struct song_node * node, char * a, char * n) {
-    struct song_node * temp = node;
-	// right now, this only tests for the first letter
-	// will need to write a new function to check the entire word
+  struct song_node * temp = node;
+  // right now, this only tests for the first letter
+  // will need to write a new function to check the entire word
   if (strcmp(a, temp -> artist) <= 0) {
-		return insert_name(node, a, n);
-	}
-	else {
-		return insert_order(node -> next, a, n);
-	}
+    return insert_name(node, a, n);
+  }
+  else {
+    return insert_order(node -> next, a, n);
+  }
 }
 
 //helper func for insert_order
 struct song_node * insert_name(struct song_node * node, char * a, char * n) {
   struct song_node * temp = node;
-	// right now, this only tests for the first letter
-	// will need to write a new function to check the entire word
-	if (strcmp(n, temp->name) <= 0) {
-		return insert_front(node, a, n);
-	}
-	else {
-		return insert_name(node -> next, a, n);
-	}
+  // right now, this only tests for the first letter
+  // will need to write a new function to check the entire word
+  if (strcmp(n, temp->name) <= 0) {
+    return insert_front(node, a, n);
+  }
+  else {
+    return insert_name(node -> next, a, n);
+  }
 }
 
 // free the linked list of the given node
 struct song_node * free_list(struct song_node * node) {
-    if (node -> next == NULL) {
-        free(node);
-    } else {
-        free_list(node -> next);
-        free(node);
-    }
+  if (node -> next == NULL) {
+    free(node);
+  } else {
+    free_list(node -> next);
+    free(node);
+  }
 }
--- linked_list.h
+++ linked_list.h
@@ -1,7 +1,7 @@
 struct song_node{
-    char name[100];
-    char artist[100];
-    struct song_node *next;
+  char name[100];
+  char artist[100];
+  struct song_node *next;
 };
 
 struct song_node * table[27];
